c9d6af6e4e0bfc54f3a63799a12be280945e54f8
dengjin <yezj0010@163.com>
2024-03-31 20:47:07 +0800

    增加内嵌zookeeper服务，不需要依赖外部zk服务

--- rpc-demo-consumer/src/test/java/cc/yezj/rpc/demo/consumer/RpcDemoConsumerApplicationTests.java
+++ rpc-demo-consumer/src/test/java/cc/yezj/rpc/demo/consumer/RpcDemoConsumerApplicationTests.java
@@ -1,5 +1,6 @@
 package cc.yezj.rpc.demo.consumer;
 
+import cc.yezj.rpc.core.test.TestZKServer;
 import cc.yezj.rpc.demo.provider.RpcDemoProviderApplication;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,26 +9,50 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@SpringBootTest(classes = {RpcDemoConsumerApplication.class})
 class RpcDemoConsumerApplicationTests {
 
-    static ApplicationContext context;
+    static ApplicationContext context1;
+    static ApplicationContext context2;
+
+    static TestZKServer zkServer = new TestZKServer();
 
     @BeforeAll
-    static void init(){
-        context = SpringApplication.run(RpcDemoProviderApplication.class,
-                "--server.port=9104", "--logging.level.cc.yezj.rpc=debug");
+    static void init() {
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============     ZK2182    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        zkServer.start();
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============      P8094    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        context1 = SpringApplication.run(RpcDemoProviderApplication.class,
+                "--server.port=9104", "--kkrpc.zkServer=localhost:2182",
+                "--logging.level.cc.yezj.rpc=info");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============      P8095    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        context2 = SpringApplication.run(RpcDemoProviderApplication.class,
+                "--server.port=9105", "--kkrpc.zkServer=localhost:2182",
+                "--logging.level.cc.yezj.rpc=info");
     }
 
     @Test
     void contextLoads() {
-        System.out.println("consumer start .....");
+        System.out.println(" ===> aaaa  .... ");
     }
 
-
     @AfterAll
-    static void destroy(){
-        SpringApplication.exit(context, () -> 1);
+    static void destory() {
+        SpringApplication.exit(context1, () -> 1);
+        SpringApplication.exit(context2, () -> 1);
+        zkServer.stop();
     }
 
 }
