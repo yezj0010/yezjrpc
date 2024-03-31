2cb1c0a2ea8e91d9cfb8a74295ad8b047c99b130
dengjin <yezj0010@163.com>
2024-03-31 20:01:56 +0800

    consumer单元测试优化

--- rpc-demo-consumer/src/test/java/cc/yezj/rpc/demo/consumer/RpcDemoConsumerApplicationTests.java
+++ rpc-demo-consumer/src/test/java/cc/yezj/rpc/demo/consumer/RpcDemoConsumerApplicationTests.java
@@ -1,13 +1,32 @@
 package cc.yezj.rpc.demo.consumer;
 
+import cc.yezj.rpc.demo.provider.RpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class RpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void init(){
+        context = SpringApplication.run(RpcDemoProviderApplication.class, "--server.port=9104");
+    }
+
     @Test
     void contextLoads() {
+        System.out.println("consumer start .....");
+    }
+
+
+    @AfterAll
+    static void destroy(){
+        SpringApplication.exit(context, () -> 1);
     }
 
 }
