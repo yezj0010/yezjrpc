dc2b518ca0c22247c8a8aa82e72e25976443cf33
dengjin <yezj0010@163.com>
2024-04-04 21:45:12 +0800

    修改测试类

--- rpc-demo-provider/src/test/java/cc/yezj/rpc/demo/provider/RpcDemoProviderApplicationTests.java
+++ rpc-demo-provider/src/test/java/cc/yezj/rpc/demo/provider/RpcDemoProviderApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class rpcDemoProviderApplicationTests {
+class RpcDemoProviderApplicationTests {
 
 	static TestZKServer zkServer = new TestZKServer();
 
